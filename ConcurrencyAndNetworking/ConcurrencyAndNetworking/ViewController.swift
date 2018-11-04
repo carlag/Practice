707426fb3be7e158782e087be9529c07ce713f3c
Carla <carla.goldstein@ayudaheuristics.com>
2018-11-04 09:39:47 +0000

    ConcurrencyAndNetworking | Added initial project with concurrency issues

---
+++ ConcurrencyAndNetworking/ConcurrencyAndNetworking/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ConcurrencyAndNetworking
+//
+//  Created by Carla on 04/11/2018.
+//  Copyright © 2018 Carla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var calculatePrimeNumbersButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func calculateButtonTapped(_ sender: Any) {
+        for number in 0 ... 100_000_000 {
+            let isPrime = isPrimeNumber(number: number)
+            print("\(number) is prime: \(isPrime)")
+        }
+    }
+    
+    func isPrimeNumber(number: Int) -> Bool {
+        if number <= 1 {
+            return false
+        }
+        
+        if number <= 3 {
+            return true
+        }
+        
+        var i = 2
+        while i * i <= number {
+            if number % i == 0 {
+                return false
+            }
+            i = i + 2
+        }
+        return true
+    }
+    
+}
+
